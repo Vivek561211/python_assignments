d2951c2380ec1fe341f9fac6a50d67ec44a0992c
Vivek561211 <139207307+Vivek561211@users.noreply.github.com>
2023-07-11 19:11:32 +0530

    Delete Python directory

--- Python/Assignment 4/data.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data=pd.read_csv("/Users/vivekborade/Downloads/Python/Assignment 4/grainsales.csv")
-#print(data)
-df=pd.DataFrame(data)
-print(df)
-
-# 1)Which was the best month for sales? How much was earned that month?
--- Python/datatransform.py
+++
@@ -1,7 +0,0 @@
-#converting rupees into dollar
-def dollar(salary):
-    a=salary/81.80
-    return a
-
-salary=int(input("Enter your salary:"))
-print(dollar(salary))
